767e7859bd02c12a50e98ca89d23fcf149e2624c
raychung0520 <raychung0520@gmail.com>
2022-05-11 00:14:19 -0700

    Loading views

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,15 +1,18 @@
 <?php
-    class Pages {
+    class Pages extends Controller {
         public function __construct()
         {
             // echo 'Page loaded';
         }
 
         public function index() {
-            
+            $data = [
+                'title' => 'Welcome'
+            ];
+            $this->view('pages/index', $data);
         }
 
-        public function about($id) {
-            echo $id;
+        public function about() {
+            $this->view('pages/about');
         }
     }
