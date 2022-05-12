a2176b0d7ef10f92e46b05a198a812b56430d232
raychung0520 <raychung0520@gmail.com>
2022-05-11 23:14:11 -0700

    added database class for the model

---
+++ app/models/Post.php
@@ -0,0 +1,18 @@
+<?php
+class Post {
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database; 
+    }
+
+    public function getPosts() {
+        $this->db->query("SELECT * FROM posts");
+
+        $results = $this->db->resultSet();
+
+        return $results;
+    
+    }
+}
